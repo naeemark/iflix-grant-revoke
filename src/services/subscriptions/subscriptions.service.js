b9afbe0dfa583399e865da0725056aab61f5a568
Naeem <naeemark@gmail.com>
2020-01-07 14:52:48 +0800

    testing logic

--- src/services/subscriptions/subscriptions.service.js
+++ src/services/subscriptions/subscriptions.service.js
@@ -5,10 +5,17 @@ let accountsData = null;
 let amazecomData = null;
 let wondertelData = null;
 
+const restructure = async (providerName, providerData) => {
+  const data = [];
+  providerData.grants.forEach((element) => { element['provider'] = providerName; element['type'] = 'grant'; data.push(element); });
+  providerData.revocations.forEach((element) => { element['provider'] = providerName; element['type'] = 'revocation'; data.push(element); });
+  return data;
+}
+
 const getInputs = async () => {
   accountsData = await getAccounts();
-  amazecomData = await getAmazecom();
-  wondertelData = await getWondertel();
+  amazecomData = await restructure('Amazecom', await getAmazecom());
+  wondertelData = await restructure('Wondertel', await getWondertel());
 };
 
 const findAmazecom = async (accountNumber) => {
@@ -25,14 +32,49 @@ const findWondertel = async (accountNumber) => {
   return hours;
 };
 
+const getGrantsRevokes = async (accountNumber) => {
+  const gr = []
+  amazecomData.forEach((element) => { if (element.number === accountNumber) { gr.push(element); } });
+  wondertelData.forEach((element) => { if (element.number === accountNumber) { gr.push(element); } });
+  gr.sort((x, y) => new Date(x.date) - new Date(y.date))
+  return gr;
+
+
+  // const Amazecom = await findAmazecom(accountNumber);
+  // const Wondertel = await findWondertel(accountNumber);
+};
+
 const getSubscriptions = async () => {
   await getInputs();
 
   const subscriptions = {};
-  for (const user of accountsData.users) {
-    const Amazecom = await findAmazecom(user.number);
-    const Wondertel = await findWondertel(user.number);
-    subscriptions[user.name] = { Amazecom, Wondertel }
+  for (const user of accountsData.users.slice(0, 1)) {
+    const grs = await getGrantsRevokes(user.number);
+    console.log(grs);
+    const u = { owner: null, start: null, end: null, days: 0 }
+    grs.forEach((gr) => {
+      if (gr.type === 'grant' && u.owner === null) {
+        u.owner = gr.provider;
+        if (u.start === null)
+          u.start = new Date(gr.date);
+        const endDate = new Date(gr.date)
+        endDate.setMonth(u.start.getMonth() + ((gr.period) ? gr.period : 0));
+        u.end = endDate;
+        var days = (u.end.getTime() - u.start.getTime()) / (1000 * 3600 * 24);
+        u.days = days;
+      }
+      else if (gr.type === 'grant' && u.owner === gr.provider) {
+        const endDate = new Date(u.end)
+        endDate.setMonth(endDate.getMonth() + ((gr.period) ? gr.period : 0));
+        var days = (endDate.getTime() - u.start.getTime()) / (1000 * 3600 * 24);
+        u.end = endDate;
+        u.days = days;
+      }
+      else if (gr.type === 'revocation' && u.owner === gr.provider) {
+        u.owner = null
+      }
+    });
+    subscriptions[user.name] = { [u.owner]: u.days }
   }
   return { subscriptions };
 };
