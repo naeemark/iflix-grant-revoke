9153bf8b39bbf90487942a6649e1c76b29cf404b
Naeem <naeemark@gmail.com>
2020-01-07 23:35:35 +0800

    refactoring

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
-const calculateDays = async () => {
-
+const calculateDays = async (startDate, endDate) => {
+  return Math.floor((endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24));
 };
 
 module.exports = {
