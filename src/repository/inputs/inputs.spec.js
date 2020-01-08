4c4744ef9bfff6d112a1b3beb09205217d4453d2
Naeem <naeemark@gmail.com>
2020-01-08 15:20:27 +0800

    adds unit tests

--- src/repository/inputs/inputs.spec.js
+++ src/repository/inputs/inputs.spec.js
@@ -1,3 +1,5 @@
+const { getAccounts, getAmazecom, getWondertel } = require('./inputs.repository')
+
 describe('Service - inputs', () => {
   beforeEach(() => { });
 
@@ -5,8 +7,43 @@ describe('Service - inputs', () => {
     jest.resetAllMocks();
   });
 
-  it('checksum', async (done) => {
-    expect(1 + 1).toEqual(2);
-    done();
+  it('pass if getAccounts has users array', async () => {
+    return await getAccounts().then((result) => {
+      expect(result).toBeObject();
+      expect(result).toContainKey('users');
+      expect(result.users).toBeArray();
+    });
+  });
+
+  it('pass if getAmazecom has grants', async () => {
+    return await getAmazecom().then((result) => {
+      expect(result).toBeObject();
+      expect(result).toContainKey('grants');
+      expect(result.grants).toBeArray();
+    });
+  });
+
+  it('pass if getAmazecom has revokes', async () => {
+    return await getAmazecom().then((result) => {
+      expect(result).toBeObject();
+      expect(result).toContainKey('revocations');
+      expect(result.revocations).toBeArray();
+    });
+  });
+
+  it('pass if getWondertel has grants', async () => {
+    return await getWondertel().then((result) => {
+      expect(result).toBeObject();
+      expect(result).toContainKey('grants');
+      expect(result.grants).toBeArray();
+    });
+  });
+
+  it('pass if getWondertel has revokes', async () => {
+    return await getWondertel().then((result) => {
+      expect(result).toBeObject();
+      expect(result).toContainKey('revocations');
+      expect(result.revocations).toBeArray();
+    });
   });
 });
